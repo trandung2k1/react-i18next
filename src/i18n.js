9ae8b65eb73803603f4b10fde7affded16575a2c
trandung2k1 <trandungitksnb2001@gmail.com>
2024-05-19 11:12:06 +0700

    init commit

---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import translationEN from './locales/en/translation.json';
+import translationVI from './locales/vi/translation.json';
+
+const resources = {
+    en: {
+        translation: translationEN
+    },
+    vi: {
+        translation: translationVI
+    }
+};
+i18n
+  // load translation using http -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-http-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources,
+    fallbackLng: 'vi',
+    debug: true,
+  });
+
+export default i18n;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+);
